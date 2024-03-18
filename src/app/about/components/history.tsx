7242335d16166499c9aa0f2e4cb9364c816c9f5e
ardioputra <ardioputra.1109@gmail.com>
2024-03-18 23:56:33 +0700

    feat: add last eager loader

--- src/app/about/components/history.tsx
+++ src/app/about/components/history.tsx
@@ -3,9 +3,17 @@ import Image from "next/image";
 export default function History() {
   return (
     <div className="text-center justify-center">
-      <h1 className="text-8xl pb-10 font-bold max-[600px]:text-5xl">OUR HISTORY</h1>
+      <h1 className="text-8xl pb-10 font-bold max-[600px]:text-5xl">
+        OUR HISTORY
+      </h1>
       <div className="pb-10 flex justify-center items-center">
-      <Image alt="gedung" src="/gedung.avif" width={800} height={800}/>
+        <Image
+          loading="eager"
+          alt="gedung"
+          src="/gedung.avif"
+          width={800}
+          height={800}
+        />
       </div>
       <p>
         At OIDRA Solution, we embarked on our journey with a vision to
